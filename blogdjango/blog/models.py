2e3f20330ce88102d7c33193359f23625a9f07ae
Daniel <daniel@becher.com.br>
2022-03-08 13:43:39 -0300

    Criada a migration/tabela Post para o blog

--- blogdjango/blog/models.py
+++ blogdjango/blog/models.py
@@ -8,5 +8,7 @@ class Post(models.Model):
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
+    def __str__(self):
+        return self.title
 
